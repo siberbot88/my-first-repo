7aca0a510b69e351db528dd8a78f4352f4a82028
Mohammad Bayu Rizki <widjanarko020@gmail.com>
2025-02-08 21:51:57 +0700

    update

--- belajar git.py
+++ belajar git.py
@@ -2,9 +2,11 @@ def perkalian(a,b,c):
     return a*b*c
 
 result = perkalian(11,7,6)
-print(result)    
+print(result)   
+
+print('ini adalah contoh dari main') 
 
 
-
+#ini merupakan cas terbaru
     
     
